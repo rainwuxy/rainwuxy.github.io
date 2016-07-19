8193ca28833d2db8799da15a03dc0cfbdddbe6e1
RainWu <wxy345824684@qq.com>
2016-07-19 10:01:09 +0800

    Add files via upload

---
+++ JS/works.js
@@ -0,0 +1,9 @@
+/**
+ * Created by hp on 2016/7/10.
+ */
+$(document).ready(function(){
+    w=document.documentElement.clientWidth;
+    imgwidth=(w*0.63-22)+"px";;
+    //$(".work-img").width(imgwidth);
+    $(".work-img").css("width",imgwidth);
+});
